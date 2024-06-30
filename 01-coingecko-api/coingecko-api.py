66b6b92cc937e95b935cf960a0907955cdc6c3c5
Nat Sanvanitpattana <130334679+Imppel-9704@users.noreply.github.com>
2024-06-30 10:26:25 +0000

    Updated code and added docker-compose

--- 01-coingecko-api/coingecko-api.py
+++ 01-coingecko-api/coingecko-api.py
@@ -1,26 +1,81 @@
 import requests
 import os
+import psycopg2
 from dotenv import load_dotenv
+from typing import NewType
 
-list_of_crypto = ['ethereum', 'bitcoin', 'solana', 'dogecoin', 'arbitrum', 'binancecoin', 'ripple', 'shiba-inu', 'dai', 'matic-network', 'tron', 'avalanche-2']
+PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
+PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
-headers = { 'x-cg-demo-api-key': os.getenv("MY_API_KEY")}
+table_create_statement = """
+    CREATE TABLE IF NOT EXISTS crypto_currencies (
+        name VARCHAR(50) PRIMARY KEY,
+        price DECIMAL(20, 10),
+        umarket_cap DECIMAL(20, 2),
+        24h_vol DECIMAL(20, 2),
+        24h_change DECIMAL(5, 2),
+        last_updated_at TIMESTAMP
+    )
+"""
 
-for crypto in list_of_crypto:
-    url = "https://api.coingecko.com/api/v3/simple/price"
-    params = {  
-        "ids": crypto,
-        "vs_currencies": "USD",
-        "include_market_cap":'true', 
-        "include_24hr_vol":'true', 
-        "include_24hr_change":'true', 
-        "include_last_updated_at":'true'
-    }
+def create_table(cur: PostgresCursor, conn: PostgresConn) -> None:
+    """
+    Creates table using `table_create_statement`
+    """
+    cur.execute(table_create_statement)
+    conn.commit()
 
-    # Replace 'YOUR_API_KEY' with your actual API key
+def insert_data(cur: PostgresCursor, conn: PostgresConn, data):
+    """
+    Inserts data into the `crypto_currencies` table
+    """
+    for d in data:
+        for name, info in d.items():
+            insert_statement = """
+                INSERT INTO crypto_currencies (
+                    name, 
+                    price, 
+                    market_cap, 
+                    24h_vol, 
+                    24h_change, 
+                    last_updated_at
+                ) VALUES (%s, %s, %s, %s, %s, TO_TIMESTAMP(%s))
+                ON CONFLICT (name) DO NOTHING;
+            """
+            cur.execute(insert_statement, (name, info['usd'], info['usd_market_cap'], info['usd_24h_vol'], info['usd_24h_change'], info['last_updated_at']))
+        conn.commit()
 
-    response = requests.get(url, params= params, headers=headers)
+def request_data(coin_name):
+    headers = { 'x-cg-demo-api-key': os.getenv("MY_API_KEY")}
+    list_data = []
+    for name in coin_name:
+        url = "https://api.coingecko.com/api/v3/simple/price"
+        params = {  
+            "ids": name,
+            "vs_currencies": "USD",
+            "include_market_cap":'true', 
+            "include_24hr_vol":'true', 
+            "include_24hr_change":'true', 
+            "include_last_updated_at":'true'
+        }
 
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
+        response = requests.get(url, params= params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            list_data.append(data)
+    return list_data
+
+if __name__ == "__main__":
+    list_of_crypto = ['ethereum', 'bitcoin', 'solana', 'notcoin']
+
+    try:
+        conn = psycopg2.connect(
+            "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
+        )
+        cur = conn.cursor()
+        data = request_data(list_of_crypto)
+        create_table(cur, conn)
+        insert_data(cur, conn, data)
+
+    except Exception as e:
+        print(f"Error: {e}")
